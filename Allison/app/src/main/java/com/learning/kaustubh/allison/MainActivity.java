e78ff39373266dbc860ee51a0c8202bb1eef7ab0
kaustubhhiware <hiwarekaustubh@googlemail.com>
2016-12-12 21:36:50 +0530

    FFS allison -_-

---
+++ Allison/app/src/main/java/com/learning/kaustubh/allison/MainActivity.java
@@ -0,0 +1,93 @@
+package com.learning.kaustubh.allison;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.graphics.Color;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    // this code won't work in android studio 2.2.2, however written for the sake of completeness
+    // Layout
+    RelativeLayout rl = new RelativeLayout(this);
+    rl.setBackgroundColor(Color.GREEN);
+
+    RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams{
+        RelativeLayout.LayoutParams.WRAP_CONTENT,
+        RelativeLayout.LayoutParams.WRAP_CONTENT,// automatic adjust
+    }
+
+    buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+    buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+    // Button
+    Button redButton = new Button(this);
+    redButton.setText("Click me Daddy");
+    redButton.setBackgroundColor(Color.RED);
+
+    // add widget to layout
+    addContentView(rl, redButton);
+
+    // set to current layout
+    setContentView(rl);
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+}
